3c3958a4779f00dd61370875db2490d29559cfbc
boev <boev@rg.ru>
2024-10-03 17:35:11 +0200

    add retry

--- main.go
+++ main.go
@@ -11,14 +11,16 @@ import (
 
 func getResourceUsage() (string, error) {
 	response, err := http.Get("https://srv.msk01.gigacorp.local/_stats")
-
 	if err != nil {
 		return "", err
 	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("unexpected HTTP status: %s", response.Status)
+	}
 
 	body, err := io.ReadAll(response.Body)
-	response.Body.Close()
-
 	if err != nil {
 		return "", err
 	}
@@ -30,24 +32,29 @@ func main() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
+	errorCount := 0
+
 	for range ticker.C {
 		res, err := getResourceUsage()
-
 		if err != nil {
-			fmt.Println(err)
-			return
+			errorCount++
+		} else {
+			status, err := server.ParseStatus(res)
+			if err != nil {
+				errorCount++
+			} else {
+				errorCount = 0
+
+				status.CheckLoadAverage()
+				status.CheckMemoryUsage()
+				status.CheckDiskSpace()
+				status.CheckNetworkUsage()
+			}
 		}
 
-		status, err := server.ParseStatus(res)
-
-		if err != nil {
-			fmt.Println(err)
-			return
+		if errorCount >= 3 {
+			fmt.Println("Unable to fetch server statistic.")
+			errorCount = 0
 		}
-
-		status.CheckLoadAverage()
-		status.CheckMemoryUsage()
-		status.CheckDiskSpace()
-		status.CheckNetworkUsage()
 	}
 }
