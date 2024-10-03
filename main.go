baed009068496931c677fdf003d8e3c32e2c0f7e
boev <boev@rg.ru>
2024-10-03 16:32:27 +0200

    add get req

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"time"
 )
 
+func getResourceUsage() error {
+	res, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
+
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(body)
+
+	return nil
+}
+
 func main() {
-	ticker := time.NewTicker(5 * time.Second)
+	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
-	for t := range ticker.C {
-		fmt.Println(t)
+	for range ticker.C {
+		getResourceUsage()
 	}
 }
