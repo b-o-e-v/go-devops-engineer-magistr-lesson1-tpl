f9473dbb5edfe68afd46d3ff7adfd855e3142c54
boev <boev@rg.ru>
2024-10-03 16:15:57 +0200

    init ticker

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for t := range ticker.C {
+		fmt.Println(t)
+	}
+}
